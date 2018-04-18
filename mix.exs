2ada42e6382712099f4837dcfca1c7a1cc226b32
Jean Parpaillon <jean.parpaillon@free.fr>
2018-04-18 17:56:39 +0200

    Upgrade version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Nox.MixProject do
 
   def project, do: [
     app: :nox,
-    version: "0.1.0",
+    version: "0.2.0",
     elixir: ">= 1.3.0",
     start_permanent: Mix.env() == :prod,
     description: description(),
